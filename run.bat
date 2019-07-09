9e598f3cd828231d43498934b1198804b0d916db
gateswell <caoshuhuan@yeah.net>
2019-07-09 11:03:30 +0800

    main batch

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-python D:\MGIseqerFilesCollect\MGIseqerOutputCollect.py V300018621 20190101 D:\tmp
+python D:\MGIseqerFilesCollect\MGIseqerOutputCollect.py V300018621 20190101 D:\CopyData
 @echo ======== collect finished =============
