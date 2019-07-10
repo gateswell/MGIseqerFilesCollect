75645a03db05bda3fb30dbf51e161b72ff5e5b8e
gateswell <caoshuhuan@yeah.net>
2019-07-10 19:53:25 +0800

    collect some MGI sequencer files to destination directory

--- MGIseqerOutputCollect.py
+++ MGIseqerOutputCollect.py
@@ -9,21 +9,26 @@ def help_info():
 	info='''
 ============================================================================
 Usage:
-	python MGIseqerOutputCollect.py <flowcell name> <ending date> <outdir>
+	python MGIseqerOutputCollect.py <flowcell name> <ending date> <cycle:S001-S010,S101,S120|None> [outdir]
 Example:
-	python MGIseqerOutputCollect.py V300018621 20190101 D:\CopyData
+	python MGIseqerOutputCollect.py V300018621 20190101 S001 D:\CopyData
 Version:
-	V1.0.0.0
+	V1.0.1
 Note:
 	The Script can be run by python2 or python3 
 	outdir will be created if outdir unexists, the default outdir is D:\CopyData
+	write S001-S010,S101,S120 if wanna cycle 1 to 10 and cycle 101, 120 instead of writing None when no need copy any cycle
 	contact caoshuhuan@yeah.net if any bug happened during runing.
+Modify:
+	1. add parameter: cycle 
+	2. delete some redundant scripts
+	3. fix some bugs
 ============================================================================
 '''
 	print(info.rstrip()) 
 	exit(0)
 	
-if len(sys.argv)<3:	#outdir can be dismissed
+if len(sys.argv)<4:	#outdir can be dismissed
 	help_info()
 	
 def print_time():
@@ -40,8 +45,33 @@ def get_days(date,num=10):	#date format:20190101
 		#print format_befday
 	return alldays
 
+def collect_cycles(cycle):	#S001-S003,S100,S152
+	cycles=[]
+	if cycle.upper() != 'NONE':
+		if re.search(r',',cycle):
+			for tmp in cycle.split(','):
+				if re.search(r'-',tmp):	#S001-S003
+					sta,end=tmp.split('-')	#S001 S003
+					sta=int(sta.lstrip('S'))	#001
+					end=int(end.lstrip('S'))	#003
+					for i in range(sta,end+1):
+						i="{:0>3d}".format(i)	#001 002 003
+						cycles.append('S'+i)		#S001 S002 S003
+				else:
+					cycles.append(tmp)
+		elif re.search(r'-',cycle):
+			sta,end=cycle.split('-')
+			sta=int(sta.lstrip('S'))
+			end=int(end.lstrip('S'))
+			for i in range(sta,end+1):
+				i="{:0>3d}".format(i)
+				cycles.append('S'+i)
+	else:
+		cycles=['None']
+	return cycles
+		
 # copy chip png and tif files
-def copy_DataThumbFOVFigs(dir,chipname,outdir):
+def copy_DataThumbFOVFigs(dir,chipname,cycles,outdir):
 	try :
 		for lane in os.listdir(dir):
 			#if lane != 'Metrics':
@@ -49,26 +79,27 @@ def copy_DataThumbFOVFigs(dir,chipname,outdir):
 				if not os.path.exists(outdir+'\\'+chipname+'\\'+lane):
 					os.makedirs(outdir+'\\'+chipname+'\\'+lane)
 					
-				for files in os.listdir(dir+'\\'+lane+'\\S001'):
-					#if files.startswith('Thumbnail'):	#copy thumbnail figures
-					if re.search(r'C004R036|Thumbnail',files):
-						fullname=dir+'\\'+lane+'\\S001\\'+files
-						outputname=outdir+'\\'+chipname+'\\'+lane+'\\'+files
-						if not os.path.exists(outputname):
-							copyfile(fullname,outputname)
-							sys.stdout.write(print_time()+'Copy file: '+fullname+'\n')
-					else:
-						#sys.stdout.write('No such files: '+files+' \n')
-						continue
-					'''if re.search(r'C004R036',files):	#copy FOV C004R036 figures
-						fullname=dir+'\\'+lane+'\\S01\\'+files
-						outputname=outdir+'\\'+chipname+'\\'+lane+'\\'+files
-						if not os.path.exists(outputname):
-							copyfile(fullname,outputname)
-							sys.stdout.write(print_time()+'Copy file: '+fullname+'\n')
-					else:
-						sys.stdout.write('No such files: '+files+' \n')
-						continue'''
+				if len(cycles) >= 1 and cycles[0].upper() != 'NONE': #&& cycle.upper() != 'NONE':
+					for cycle in cycles:
+						if not os.path.exists(outdir+'\\'+chipname+'\\'+lane+'\\'+cycle):	#add cycle directory
+							os.makedirs(outdir+'\\'+chipname+'\\'+lane+'\\'+cycle)
+						#for files in os.listdir(dir+'\\'+lane+'\\S001'):	v1.0.0
+						for files in os.listdir(dir+'\\'+lane+'\\'+cycle):	#add cycle choose parameter
+							#if files.startswith('Thumbnail'):	#copy thumbnail figures
+							if re.search(r'C004R036|Thumbnail',files):
+								#fullname=dir+'\\'+lane+'\\S001\\'+files
+								fullname=dir+'\\'+lane+'\\'+cycle+'\\'+files
+								outputname=outdir+'\\'+chipname+'\\'+lane+'\\'+cycle+'\\'+files
+								if not os.path.exists(outputname):
+									copyfile(fullname,outputname)
+									sys.stdout.write(print_time()+'Copy file: '+fullname+'\n')
+							else:
+								#sys.stdout.write('No such files: '+files+' \n')
+								continue
+				elif cycles[0].upper() == 'NONE':
+					continue
+				else:
+					sys.stdout.write('wrong parameter been set! please check the cycle string, only None or format like: S001-S007,S100,S120 allowed!\n')
 			else:
 				continue
 	except IOError as e:
@@ -165,11 +196,12 @@ def main():
 	dirZ='C:\\ZebraCallV2\\1.0.7.197'
 	date=sys.argv[2]
 	outdir=''
-	if len(sys.argv) ==4:
-		outdir=sys.argv[3]
-	elif len(sys.argv) ==3:
+	cycles=sys.argv[3]
+	if len(sys.argv) ==5:
+		outdir=sys.argv[4]
+	elif len(sys.argv) ==4:
 		outdir='D:\\CopyData'
-	copy_DataThumbFOVFigs(dirD,chipname,outdir)
+	copy_DataThumbFOVFigs(dirD,chipname,cycles,outdir)
 	copy_Metrics(dirM,chipname,outdir)
 	copy_ResultStatHtml(dirR,chipname,outdir)
 	copy_BGI_logs(dirL,date,chipname,outdir)
