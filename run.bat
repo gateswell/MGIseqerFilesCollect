6671730edf1f2ade7e6a8ae756b81e4918e8b54a
gateswell <caoshuhuan@yeah.net>
2019-07-11 15:17:46 +0800

    main batch file

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-@echo ========== collect start ==============
-python D:\MGIseqerFilesCollect-master\MGIseqerOutputCollect.py V300018621 20190101 D:\CopyData
+@echo ========== collect start ============== 
+python D:\MGIseqerFilesCollect-master\MGIseqerOutputCollect.py V300018621 20190101 S001-S100,S151,S300 D:\CopyData
 @echo ======== collect finished =============
